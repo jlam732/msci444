2df3a385bb3ea33cec7a29dc661b27750acb7a65
Jonathan Lam <charon732@gmail.com>
2015-03-18 23:06:44 -0400

    json encode result

--- php/login.php
+++ php/login.php
@@ -9,19 +9,20 @@
 	    // set the PDO error mode to exception
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	    $sql = "SELECT id, first_name, last_name, password FROM user WHERE alias='" . $_POST["alias"] . "'";
+	    $sql = "SELECT * FROM user WHERE alias='" . $_POST["alias"] . "'";
 	    $result = $conn->query($sql);
 	    $array = $result->fetchAll();
 	    if(count($array) == 1) {
 	        if($array[0]["password"] == $_POST["password"]) {
-		    echo "yayyyyy";
+		    header('Content-Type: application/json');
+		    echo json_encode($array[0]);
 		} else {
 		    echo "Incorrect password, please change and try again.";
 		}
 	    } else {
 		echo "Username not found, please try another account.";
 	    }
-	    $conn->close();
+	    //$conn->close();
 	} catch(PDOException $e) {
 	    	echo $sql . "<br>" . $e->getMessage();
 	}
