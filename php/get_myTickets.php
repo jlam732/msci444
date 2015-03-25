5b9b718e4a1efcf8a04488c661fc40eba0a3d7dc
jlam732 <charon732@gmail.com>
2015-03-25 16:10:19 -0400

    debugging

--- php/get_myTickets.php
+++ php/get_myTickets.php
@@ -6,8 +6,9 @@
 	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	    $sql = "";
+	    echo $_SESSION;
 	    if($_SESSION["type"] == 1) {
-	    	$sql = "SELECT * FROM ticket WHERE client ='" . $_SESSION["id"] . "'";
+	    	$sql = "SELECT * FROM ticket WHERE creator ='" . $_SESSION["id"] . "'";
 	    } else {
 	    	$sql = "SELECT * FROM ticket WHERE technican ='" . $_SESSION["id"] . "'";
 	    }
