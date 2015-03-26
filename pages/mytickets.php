3dbc83f9bc446bd4b0ab6d2023f066f40aa31d0d
ProGrammar101 <ngke81@gmail.com>
2015-03-26 16:52:19 -0400

    Update mytickets.php

--- pages/mytickets.php
+++ pages/mytickets.php
@@ -51,7 +51,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="myTickets.php">SB Admin v2.0</a>
+                <a class="navbar-brand" href="index.php">MYSYDE IT Consulting Group</a>
             </div>
             <!-- /.navbar-header -->
 
@@ -101,17 +101,6 @@
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
                         <li>
                             <a href="mytickets.php"><i class="fa fa-dashboard fa-fw"></i> My Tickets</a>
                         </li>
