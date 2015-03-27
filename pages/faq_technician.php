51a39370c6af1cc763db48895c3a708e68c140c2
jlam732 <charon732@gmail.com>
2015-03-26 21:36:08 -0400

    forgot a few routes

--- pages/faq_technician.php
+++ pages/faq_technician.php
@@ -45,7 +45,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="mytickets.php">MYSYDE IT Consulting Group</a>
+                <a class="navbar-brand" href="mytickets_technician.php">MYSYDE IT Consulting Group</a>
             </div>
             <!-- /.navbar-header -->
 
