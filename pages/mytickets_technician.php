9d7ad46af49a91a2c89611a85149180b47a7a427
jlam732 <charon732@gmail.com>
2015-03-27 13:46:59 -0400

    accidentally deleted custom css

--- pages/mytickets_technician.php
+++ pages/mytickets_technician.php
@@ -17,6 +17,12 @@
     <!-- MetisMenu CSS -->
     <link href="../bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
 
+    <!-- Timeline CSS -->
+    <link href="../dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+    
     <!-- DataTables CSS -->
     <link href="../bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet">
 
