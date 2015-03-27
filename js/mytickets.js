49335731d3edf77d8b8914434a8bac126eec3b00
jlam732 <charon732@gmail.com>
2015-03-27 12:09:58 -0400

    testing for client id

--- js/mytickets.js
+++ js/mytickets.js
@@ -7,20 +7,25 @@ $(document).ready(function() {
 	    var tickets = JSON.parse(data);
             var dataTable = $('#dataTables-example tbody');
             for(var index = 0; index < tickets.length; index++) {
-		var ticket = tickets[index];
-		ticket["first_name"] += " " + ticket["last_name"];
-		delete ticket["last_name"];
+        		var ticket = tickets[index];
+        		ticket["first_name"] += " " + ticket["last_name"];
+        		delete ticket["last_name"];
                 var tr = "<tr " + (index % 2 == 0 ? 'class="odd">' : 'class="even">');
                 for (var key in ticket) {
                     if (ticket.hasOwnProperty(key)) {
                         tr += "<td>" + ticket[key] + "</td>";
                     }
                 }
+
+                //make the button
+                alert(<?php session_start(); echo $_SESSION["id"]; ?>);
                 dataTable.append(tr + "</tr>");
             }
             $('#dataTables-example').DataTable({
       	      responsive: true
-    	    }); 
+    	    });
+
+
         },
         error: function(error) {
             console.log(error);
