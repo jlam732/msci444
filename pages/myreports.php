f65ce049d270935f155fccf64eb9e1c03d4e0e60
jlam732 <charon732@gmail.com>
2015-03-28 15:35:51 -0400

    changed title

--- pages/myreports.php
+++ pages/myreports.php
@@ -120,7 +120,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Unassigned Tickets</h1>
+                    <h1 class="page-header">My Reports</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
