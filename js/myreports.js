165424c7731be150eabc382d96a42441ddffab58
Rajat Shah <rajatshah92@gmail.com>
2015-03-29 17:15:26 -0400

    table display

--- js/myreports.js
+++ js/myreports.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
    	$("#techrept").click(function(){
-       console.log("sdfdsfds");
-       alert("fdgfgfdgfd"); 
+        console.log("lasjkf");
+        document.write("<table border="1"style="width:100%"><tr><td>Jill</td><td>Smith</td><td>50</td></tr><tr><td>Eve</td><td>Jackson</td><td>94</td></tr><tr><td>John</td><td>Doe</td><td>80</td></tr></table>")
     });
 
     $("#tickrept").click(function(){
