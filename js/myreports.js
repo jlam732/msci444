4865b3663cc48d90902c3913ae61d05888e63d13
Rajat Shah <rajatshah92@gmail.com>
2015-03-29 18:05:28 -0400

    table display

--- js/myreports.js
+++ js/myreports.js
@@ -1,23 +1,62 @@
 $(document).ready(function() {
     $.ajax({
-        url: '../pages/myreports.php',
+        url: '..pages/myreports.php',
         type: 'get',
         success: function(data){
-            var closedTicket=JSON.parse(data);
+            var tickets=JSON.parse(data);
 
             $("#techrept").click(function(){
-                console.log("lasjkf");
-                document.write(closedTicket);
+                document.write(tickets);
             });
-
             $("#tickrept").click(function(){
 
             });
-
-        },
-        error: function(error) {
+        }
+        error: function(error){
             console.log(error);
             $('.alert').text(error.responseText).show();
         }
-    });
+    })
 });
+
+
+
+//     $.ajax({
+//         url: '../pages/myreports.php',
+//         type: 'get',
+//         success: function(data){
+//             var closedTicket=JSON.parse(data);
+
+//             $("#techrept").click(function(){
+//                 console.log("lasjkf");
+//                 for(var index = 0; index < tickets.length; index++) {
+//                     var ticket = tickets[index];
+//                     ticket["first_name"] += " " + ticket["last_name"];
+//                     delete ticket["last_name"];
+//                     if(ticket["first_name"] == "null null") { ticket["first_name"] = ""; }
+//                     var tr = "<tr " + (index % 2 == 0 ? 'class="odd">' : 'class="even">');
+//                     for (var key in ticket) {
+//                         if (ticket.hasOwnProperty(key)) {
+//                             if(key == "id") {
+//                                 tr += '<td><button type="button" data-id="' + ticket["id"] + '" class="btn btn-primary editTicket">Edit</button>' + ticket[key] + "</td>";
+//                             } else {
+//                                 tr += "<td>" + (ticket[key] == null ? "" : ticket[key]) + "</td>";
+//                             }
+//                         }
+//                     }
+//                 dataTable.append(tr + "</tr>");
+//             }
+//             $('#dataTables-example').DataTable({responsive: true,});
+//             document.write();
+//         });
+
+// $("#tickrept").click(function(){
+
+// });
+
+// },
+// error: function(error) {
+//     console.log(error);
+//     $('.alert').text(error.responseText).show();
+// }
+// });
