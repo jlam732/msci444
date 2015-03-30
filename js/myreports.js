54c9afa8625a3e613a1a34b66c0c28fd109a4b23
Jonathan Lam <charon732@gmail.com>
2015-03-30 00:23:25 -0400

    fixing the js stuff

--- js/myreports.js
+++ js/myreports.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
+    var flag = false;
     $("#techrept").click(function() {
+        var thead = $('#technicianTable thead');
+        var tbody = $('#technicianTable tbody');
+	thead.empty();
+	tbody.empty();
         $.ajax({
             url: '../php/get_myReports.php',
             type: 'get',
             success: function(data){
                 var tickets = JSON.parse(data);
-                var thead = $('#technicianTable thead');
-                var tbody = $('#technicianTable tbody');
                 thead.append("<tr><th>Name of Technician</th><th>Number of Closed Tickets</th></tr>");
+		tr = "";
                 for(var index = 0; index < tickets.length; index++) {
                     var ticket = tickets[index];
                     ticket["first_name"] += " " + ticket["last_name"];
@@ -15,9 +19,13 @@ $(document).ready(function() {
                     if(ticket["first_name"] == "null null") { ticket["first_name"] = ""; }
                     tr+='<tr><td>' + ticket["first_name"] + '</td><td>' + ticket["tickets_closed"] + '</td></tr>';
                 }
-                $('#technicianTable').DataTable({
-                 responsive: true,
-                });
+		tbody.append(tr);
+		if(!flag) {
+			flag = true;
+		        $('#technicianTable').DataTable({
+                		 responsive: true,
+	                });
+		}
             },
             error: function(error){
                 console.log(error);
