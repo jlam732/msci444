fb8753a34e031535557a50181a21281c002e81ad
rajatshah92 <rajatshah92@hotmail.com>
2015-03-29 22:21:04 -0400

    table display

--- js/mytickets.js
+++ js/mytickets.js
@@ -5,28 +5,28 @@ $(document).ready(function() {
         success: function(data) {
 	    //put the tickets dynamically in the table using foreach
 	    var tickets = JSON.parse(data);
-        var dataTable = $('#dataTables-example tbody');
-        for(var index = 0; index < tickets.length; index++) {
-          var ticket = tickets[index];
-          ticket["first_name"] += " " + ticket["last_name"];
-          delete ticket["last_name"];
-          if(ticket["first_name"] == "null null") { ticket["first_name"] = ""; }
-          var tr = "<tr " + (index % 2 == 0 ? 'class="odd">' : 'class="even">');
-          for (var key in ticket) {
-            if (ticket.hasOwnProperty(key)) {
-                if(key == "id") {
-                    tr += '<td><button type="button" data-id="' + ticket["id"] + '" class="btn btn-primary editTicket">Edit</button>' + ticket[key] + "</td>";
-                } else {
-                    tr += "<td>" + (ticket[key] == null ? "" : ticket[key]) + "</td>";
+            var dataTable = $('#dataTables-example tbody');
+            for(var index = 0; index < tickets.length; index++) {
+        		var ticket = tickets[index];
+        		ticket["first_name"] += " " + ticket["last_name"];
+        		delete ticket["last_name"];
+                if(ticket["first_name"] == "null null") { ticket["first_name"] = ""; }
+                var tr = "<tr " + (index % 2 == 0 ? 'class="odd">' : 'class="even">');
+                for (var key in ticket) {
+                    if (ticket.hasOwnProperty(key)) {
+                        if(key == "id") {
+                            tr += '<td><button type="button" data-id="' + ticket["id"] + '" class="btn btn-primary editTicket">Edit</button>' + ticket[key] + "</td>";
+                        } else {
+                            tr += "<td>" + (ticket[key] == null ? "" : ticket[key]) + "</td>";
+                        }
+                    }
                 }
-            }
-        }
                 //make the button
                 dataTable.append(tr + "</tr>");
             }
             $('#dataTables-example').DataTable({
-             responsive: true,
-         });
+      	      responsive: true,
+    	    });
 
             $('.editTicket').click(function() {
                 var ticket_id = $(this).data("id");
